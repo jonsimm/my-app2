3e7995c85dbb0ed3608feca41fc72b71c3641e74
jonsimm <jon_simm@msn.com>
2021-12-14 08:29:10 -0800

    Add API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      let response = await fetch('api/HttpTrigger1');
+
+      console.log(response.status);
+      console.log(response.statusText);
+
+      let data = await response.text();
+      setData(data);
+    })();
+  });
   return (
-    <div className="App">Hello World</div>
+    <div className="App">{data}</div>
   );
 }
 
